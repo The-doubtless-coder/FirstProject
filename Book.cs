1cfcea6e08b6884a845ee2f0a58e0f44365ae9b2
Stealeth_Code😂 <ianwanjala4@gmail.com>
2023-11-30 10:43:00 +0300

    my first commit, thank you!

---
+++ Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Book
+    {
+        private string title;
+        private string Description;
+        private string Author;
+        private int Pages;
+        public Book(string atitle, string description, string author, int pages, string name)
+        {
+            this.title = atitle;
+            this.Description = description;
+            this.Author = author;
+            this.Pages = pages;
+            Console.WriteLine("My name is: " + name);
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                if (value == "one")
+                {
+                    title = "one";
+                }
+                else
+                {
+                    title = "league";
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Immutable;
+
+namespace FirstProject // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book booker = new Book("antepede", "good book", "mr IAN", 21, "Zarathustra");
+
+            booker.Title = "Mynster, CALL ME";
+          
+
+        }
+    }
+}
